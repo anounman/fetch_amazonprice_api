ca513f5ba03317a58ae8a314601237687a9c44a9
Anksuh Das <ankush.penteste@gmail.com>
2021-12-21 15:00:04 +0530

    Deploy V1

--- find.py
+++ find.py
@@ -4,6 +4,7 @@ import requests
 
 def get_data(url , state=False):
     try:
+        print("Find App .py")
         header = {
             "User-Agent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'}
 
